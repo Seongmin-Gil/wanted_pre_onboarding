1c60af32f1ca6a56bf67ae7fe922ee65075a78ac
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-10-10 13:37:36 +0900

    채용공고 지원 API 구현 (#13) (#14)

--- middlewares/validator/notice.validator.js
+++ middlewares/validator/notice.validator.js
@@ -3,7 +3,7 @@ const CustomError = require('../../utils/CustomError');
 
 const postNoticeSchema = Joi.object({
   body: Joi.object({
-    companyId: Joi.string().required(),
+    companyId: Joi.string().uuid().required(),
     position: Joi.string().required(),
     reward: Joi.number().integer().required(),
     content: Joi.string().required(),
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const noticeRouter = require("./notice.router");
+const noticeRouter = require('./notice.router');
+const applyRouter = require('./apply.router');
 
-router.get("/ping", (req, res) => {
-  console.log("pong");
-  res.json("pong");
+router.get('/ping', (req, res) => {
+  console.log('pong');
+  res.json('pong');
 });
 
-router.use("/notice", noticeRouter);
+router.use('/notice', noticeRouter);
+router.use('/apply', applyRouter);
 
 module.exports = router;
