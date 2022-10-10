53aa93945a15e68f2f96371beaa6f2d199f1d0e6
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-10-10 17:56:29 +0900

    fix: 주석정리 (#18) (#19)

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const noticeRouter = require('./notice.router');
 const applyRouter = require('./apply.router');
 
+//server health check
 router.get('/ping', (req, res) => {
   console.log('pong');
   res.json('pong');
 });
 
+//채용공고 관련 라우터
 router.use('/notice', noticeRouter);
+
+//회원 채용공고 지원 관련 라우터
 router.use('/apply', applyRouter);
 
 module.exports = router;
