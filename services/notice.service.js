fd9297f21b44b240e2e9b94c804545990b454ddd
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-10-10 11:13:42 +0900

    feat: 채용공고 삭제 API 구현 (#7) (#8)

--- services/notice.service.js
+++ services/notice.service.js
@@ -10,6 +10,8 @@ const insertNotice = async ({ companyId, position, reward, content, skill }) => 
     .into('notice')
     .values([{ noticeId, companyId, position, reward, content, skill }])
     .execute();
+
+  return;
 };
 
 const updateNotice = async (noticeId, { position, reward, content, skill }, db) => {
@@ -37,9 +39,35 @@ const updateNotice = async (noticeId, { position, reward, content, skill }, db) 
     .set({ position, reward, content, skill })
     .where({ noticeId })
     .execute();
+
+  return;
+};
+
+const deleteNoticeInfo = async (noticeId, db) => {
+  await db.manager
+    .createQueryBuilder('notice')
+    .select()
+    .from('notice')
+    .where({ noticeId })
+    .setLock('pessimistic_write');
+
+  const [{ valideNotice }] = await appData.query(`
+  SELECT EXISTS(
+    SELECT 
+        noticeId 
+    FROM notice 
+    WHERE noticeId = '${noticeId}'
+  ) AS valideNotice
+  `);
+
+  if (!valideNotice) throw new CustomError(404, 'INVALID_NOTICEID');
+
+  await db.manager.createQueryBuilder().delete().from('notice').where({ noticeId }).execute();
+  return;
 };
 
 module.exports = {
   insertNotice,
   updateNotice,
+  deleteNoticeInfo,
 };
